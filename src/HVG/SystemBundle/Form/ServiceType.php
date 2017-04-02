da0333588630cae2c6310bf76db08331c2332181
scar <scar@virgo.accion-sa.cl>
2017-04-02 18:14:25 -0300

    Agregar service y no funciona relacion con proveedor

---
+++ src/HVG/SystemBundle/Form/ServiceType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace HVG\SystemBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ServiceType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder 
+            ->add('name', null, array(
+                'label' => 'service.form.name',
+                'attr'  => array( 'label_col' => 4, 'widget_col' => 8 ),
+                'translation_domain' => 'HVGSystemBundle',
+            ))
+        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'HVG\SystemBundle\Entity\Service'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'hvg_systembundle_service';
+    }
+
+
+}

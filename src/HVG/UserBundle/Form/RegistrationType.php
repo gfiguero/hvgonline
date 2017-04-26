2e5764aed027500aed8c710976870f297a651e6d
gfiguero <gfiguero@gmail.com>
2017-04-26 10:54:06 -0300

    version 1 alfa para QA

--- src/HVG/UserBundle/Form/RegistrationType.php
+++ src/HVG/UserBundle/Form/RegistrationType.php
@@ -46,6 +46,13 @@ class RegistrationType extends AbstractType
                 'translation_domain' => 'HVGUserBundle',
                 'attr' => array( 'label_col' => 4, 'widget_col' => 8 ),
             ))
+            ->add('communities', null, array(
+                'label' => 'user.form.communities',
+                'expanded' => true,
+                'multiple' => true,
+                'translation_domain' => 'HVGUserBundle',
+                'attr' => array( 'label_col' => 4, 'widget_col' => 8 ),
+            ))
         ;
     }
 

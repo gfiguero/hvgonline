de34868b2d10cc0829df9152e6fcf82631a37058
gfiguero <gfiguero@gmail.com>
2017-05-19 01:54:50 -0300

    agregada gestion de archivos en requerimientos y tickets

--- src/HVG/AgentBundle/Controller/DashboardController.php
+++ src/HVG/AgentBundle/Controller/DashboardController.php
@@ -14,30 +14,17 @@ class DashboardController extends Controller
 {
     public function indexAction()
     {
-        $ticket = new Ticket();
-        $newTicketForm = $this->createNewTicketForm($ticket);
-
         $user = $this->getUser();
         $areas = $user->getAreas();
         $communities = $user->getCommunities();
         $em = $this->getDoctrine()->getManager();
         $units = $em->getRepository('HVGSystemBundle:Unit')->findBy(array('community' => $communities->toArray()));
         $statuses = $em->getRepository('HVGSystemBundle:PetitionStatus')->findBy(array('result' => array(1,2)));
-//        $tickets = $em->getRepository('HVGSystemBundle:Ticket')->findByAreaCommunity($areas, $communities);
         $tickets = $em->getRepository('HVGSystemBundle:Ticket')->findBy(array('area' => $areas->toArray(), 'unit' => $units, 'ticketstatus' => $statuses));
-        $petitions = $em->getRepository('HVGSystemBundle:Petition')->findBy(array('area' => $areas->toArray(), 'community' => $communities->toArray(), 'petitionstatus' => $statuses));
-
+        $petitions = $em->getRepository('HVGSystemBundle:Petition')->findBy(array('liability' => $user, 'community' => $communities->toArray(), 'petitionstatus' => $statuses));
         return $this->render('HVGAgentBundle:Dashboard:index.html.twig', array(
             'tickets' => $tickets,
             'petitions' => $petitions,
-            'newTicketForm' => $newTicketForm->createView(),
-        ));
-    }
-
-    private function createNewTicketForm(Ticket $ticket)
-    {
-        return $this->createForm('HVG\AgentBundle\Form\TicketFilterType', $ticket, array(
-            'action' => $this->generateUrl('agent_dashboard_findunit'),
         ));
     }
 }
